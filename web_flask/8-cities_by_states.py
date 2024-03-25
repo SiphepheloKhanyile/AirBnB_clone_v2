864cd69669683b849427f7424c9ee3466096ba9c
Siphephelo Khanyile <siphephelokhanyile@gmail.com>
2024-03-25 19:31:31 +0200

    fix: fix for server states table content from database to html

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,12 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 A script that starts a Flask web application
 """
 from flask import Flask, render_template
-from models import *
 from models import storage
 from models.state import State
-from models.city import City
 
 app = Flask(__name__)
 
